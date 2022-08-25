b06d9ca5e0a912e5b2c1d0369ce5a52b0ae1fcd5
Zia Uddin Ahmed <zia.ete10@gmail.com>
2022-08-25 07:35:12 +0600

    object initilizer syntax

--- Program.cs
+++ Program.cs
@@ -8,4 +8,22 @@ var customer = new Customer(1,"Jhon");
 Console.WriteLine(customer.Id);
 Console.WriteLine(customer.Name);
 
-customer.Orders.Add(new Order()); 
+customer.Orders.Add(new Order());
+
+// this is another way to initilize a object so that we don't have to use costructor 
+var oi = new ObejctInit
+{
+    Id = 1,
+    Name = "name",
+};
+
+Console.WriteLine(oi.Id);
+
+
+
+public class ObejctInit
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+}
