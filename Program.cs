b7fcf3a1c5feace1bbb4a13b5855c98508f132b1
Zia Uddin Ahmed <zia.ete10@gmail.com>
2022-08-25 07:16:30 +0600

    constructor

--- Program.cs
+++ Program.cs
@@ -1,43 +1,11 @@
 ﻿
-var person = new Person();
-person.Name = "john";
- person.Introduce("introduce");
-
-var p = person.Parse1("using paerse 1");
-
-var p2 = Person.Parse2("using paerse 2");
-
-p.Introduce("introduce");
-p2.Introduce("introduce");
-
-// accessModifier type Iterntifier 
-public class Person
-{
-
-    // accessModifier type Iterntifier 
-    public string Name { get; set; }
-
-    public void Introduce (string to )
-    {
-        Console.WriteLine("Hi " + to + ", I am " +Name);
-    }
-
-    // static is used so that we don't have to create new instance to access the object .
-    // below method returns an object of Person. Static method is olny accesealbe to class not object 
-    public Person Parse1 (string str)
-    {
-        var person = new Person();
-        person.Name = str;
-        return person;
-    }
-
-    public static Person Parse2(string str)
-    {
-        var person = new Person();
-        person.Name = str;
-        return person;
-    }
-}
 
 
+using Oop_Rehearse;
 
+var customer = new Customer(1,"Jhon");
+
+Console.WriteLine(customer.Id);
+Console.WriteLine(customer.Name);
+
+customer.Orders.Add(new Order()); 
