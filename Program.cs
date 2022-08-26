00d6c41f0a48435620d205117ec29dc3ba1b3118
Zia Uddin Ahmed <zia.ete10@gmail.com>
2022-08-26 14:54:04 +0600

    property

--- Program.cs
+++ Program.cs
@@ -1,25 +1,50 @@
 ﻿
 
 
-var customer = new Customer();
+var customer = new Customer(new DateTime(1993, 11, 7));
 
 
-customer.SetName("zia");
+customer.Name = "test";
+//customer.BirthDate = new DateTime(1993, 11, 7);
 
-Console.WriteLine(customer.GetName());
+Console.WriteLine(customer.Age);
 
 public class Customer 
 {
+
     private string _name;
 
-    public string GetName ()
+    public string Name { get; set; }  // c# automatically create private property called Name and get and set it 
+    public DateTime BirthDate { get; private set; } // private used so that we will set it only once and its readonly 
+
+    public Customer(DateTime birthDate)
     {
-        return _name;
-    }
-    public void SetName(string name)
-    {
-        _name = name;
+        BirthDate = birthDate;
     }
 
+    public int Age
+    {
+        get
+        {
+            var timespan = DateTime.Now - BirthDate;
+            var years = timespan.Days / 365;
+            return years;
+        }
+    }
+    /*    public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+
+        }*/
+    /*    public string GetName ()
+        {
+            return _name; 
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+    */
 
 }
--- README.md
+++ README.md
@@ -7,3 +7,6 @@ static method is used in a class so that we do don't have to create an object of
  
 # AccessModifier 
  A way to control access to a class and its member . To protect its internal data.Accessmodifiers are  Public,Private,Protected,Internal,Protected Internal 
+
+# property 
+'public string Name { get; set; }'  c# automatically create private property called Name and get and set it 
